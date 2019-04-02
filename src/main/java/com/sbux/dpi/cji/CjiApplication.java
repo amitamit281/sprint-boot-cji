a095a7869c6e6962cdaae1dfdd7bbf3736750350
Amit K Gupta <amgupta@starbucks.com>
2019-04-02 11:20:20 -0700

    first commit

---
+++ src/main/java/com/sbux/dpi/cji/CjiApplication.java
@@ -0,0 +1,13 @@
+package com.sbux.dpi.cji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CjiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CjiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sbux/dpi/cji/CjiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sbux.dpi.cji;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CjiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
